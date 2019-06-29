728fb9af66435564c17daa6cccc13b0c66e1d299
Kyohei Seo <kyo199333@gmail.com>
2019-06-29 12:26:52 +0900

    create users table

--- db/migrate/20190629024355_sorcery_core.rb
+++ db/migrate/20190629024355_sorcery_core.rb
@@ -1,9 +1,17 @@
 class SorceryCore < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      # sorcery
       t.string :email,            null: false
       t.string :crypted_password
       t.string :salt
+
+      t.string :first_name
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :last_name_kana
+      t.date :birthday
+      t.integer :can_view_count
 
       t.timestamps                null: false
     end
