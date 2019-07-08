c3601cd259fd079bb7ceafa1b979eded166b6363
Kyohei Seo <kyo199333@gmail.com>
2019-07-09 00:09:38 +0900

    modify

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,6 @@ class UserSessionsController < ApplicationController
   end
 
   def create
-    @user = login(params[:email], params[:password])
 
     if @user
       redirect_to root_path, notice: 'Login successful'
