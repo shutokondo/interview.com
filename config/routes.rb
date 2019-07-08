e1c1cd81c86d6d71cf4acba218878643c5bca39b
Kyohei Seo <kyo199333@gmail.com>
2019-07-09 00:08:47 +0900

    add google authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'oauths/oauth'
+  get 'oauths/callback'
   root 'top#index'
   resources :users
+  post 'oauth/callback' => 'oauths#callback'
+  get 'oauth/callback' => 'oauths#callback'
+  get 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
 end
