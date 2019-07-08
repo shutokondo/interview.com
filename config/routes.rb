bfb72e1a3f8ec89059b18966c77b28ec2e926b79
Kyohei Seo <kyo199333@gmail.com>
2019-07-09 00:09:18 +0900

    implement lgoin and logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'oauths/callback'
   root 'top#index'
   resources :users
+  resources :user_sessions, only: %i[new create destroy]
+
   post 'oauth/callback' => 'oauths#callback'
   get 'oauth/callback' => 'oauths#callback'
   get 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
+  get 'login' => 'user_sessions#new', :as => :login
+  delete 'logout' => 'user_sessions#destroy', :as => :logout
 end
